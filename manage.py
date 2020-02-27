252c3612ecc655cb9f1e1e7a3def438292bef8be
Трофимов Алексей Юрьевич <Trofimov_AU@College.mirea>
2020-02-27 19:04:16 +0300

    cli | basic crud operations | jinja2 render posts

---
+++ manage.py
@@ -0,0 +1,32 @@
+import json
+from random import choice
+from flask.cli import FlaskGroup
+from app import app
+from models import Base, add_user, add_post
+
+cli = FlaskGroup(app)
+
+
+@cli.command('reset-db')
+def reset_db():
+    Base.metadata.drop_all()
+    Base.metadata.create_all()
+
+
+@cli.command('fill-db')
+def fill_db():
+    with open('MOCK_POSTS.json') as f:
+        mock_posts = json.load(f)
+    with open('MOCK_USERS.json') as f:
+        mock_users = json.load(f)
+    
+    usernames = []
+    for user in mock_users:
+        usernames.append(user['name'])
+        add_user(**user)
+
+    for post in mock_posts:
+        add_post(username=choice(usernames), **post)
+
+if __name__ == '__main__':
+    cli()
--- models.py
+++ models.py
@@ -1,12 +1,17 @@
-# models.py
 import datetime
 from sqlalchemy import (Column, Integer, Text, DateTime, Date, 
-                        ForeignKey, create_engine, Boolean, String)
+                        ForeignKey, create_engine, Boolean, String, Table)
 from sqlalchemy.orm import Session, relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import IntegrityError
 
 engine = create_engine('sqlite:///app.sqlite', echo=True)
 Base = declarative_base(bind=engine)
+
+association_table = Table('association', Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('post_id', Integer, ForeignKey('posts.id'))
+)
 
 class Abstract(Base):
     __abstract__ = True
@@ -19,23 +24,88 @@ class User(Abstract, Base):
     email = Column(String(40), unique=True, nullable=False)
     password = Column(String(50), nullable=False)
 
-    user_links = relationship('Link')
+    user_posts = relationship('Post')
+    favorite_posts = relationship(
+        'Post', 
+        secondary=association_table, 
+        back_populates='liked_by'
+        )
 
     def __str__(self):
         return ' | '.join([self.id, self.name, self.email, self.password])
 
-class Task(Abstract, Base):
-    __tablename__ = 'tasks'
+class Post(Abstract, Base):
+    __tablename__ = 'posts'
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     title = Column(String(60), unique=True, nullable=False)
     content = Column(String(240), nullable=False)
     url = Column(Text, unique=True, nullable=False)
     rating = Column(Integer, default=0)
     tags = Column(Text, nullable=False)
-
+    
     author = relationship('User')
+    liked_by = relationship(
+        'User', 
+        secondary=association_table, 
+        back_populates='favorite_posts'
+        )
 
     def __str__(self):
         return ' | '.join([self.id, self.user_id, self.title, self.url])
 
-Base.metadata.create_all()
+
+def add_user(name, email, password):
+    engine = create_engine('sqlite:///app.sqlite', echo=True)
+    session = Session(bind=engine)
+    new_user = User(name=name, email=email, password=password)
+    try:
+        session.add(new_user)
+        session.commit()
+        session.close()
+    except IntegrityError:
+        session.close()
+        return
+
+
+def add_post(username, title, content, url, rating, tags):
+    engine = create_engine('sqlite:///app.sqlite', echo=True)
+    session = Session(bind=engine)
+    user = session.query(User).filter_by(name=username).first()
+    user_posts = user.user_posts
+    new_post = Post(
+        title=title, 
+        content=content, 
+        url=url, 
+        rating=rating, 
+        tags=tags
+        )
+
+    try:
+        user_posts.append(new_post)
+        session.commit()
+        session.close()
+    except IntegrityError:
+        session.close()
+        return
+
+
+def get_posts():
+    engine = create_engine('sqlite:///app.sqlite', echo=True)
+    session = Session(bind=engine)
+    posts = session.query(Post).all()
+    session.close()
+    return posts
+
+
+def like_post(username, post_id):
+    engine = create_engine('sqlite:///app.sqlite', echo=True)
+    session = Session(bind=engine)
+    user = session.query(User).filter_by(name=username).first()
+    post = session.query(Post).get(post_id)
+    user_fav_posts = user.favorite_posts
+    if post not in user_fav_posts:
+        user_fav_posts.append(post)
+    else:
+        user_fav_posts.remove(post)
+    session.commit()
+    session.close()
